6d304a41920660b7f4800ba588c8d122ddcf35a7
Jade Wang <wangyujue19960222@gmail.com>
2020-01-03 20:40:19 -0500

    Add TripleX

---
+++ TripleX/cmake-build-debug/CMakeFiles/TripleX.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Jade Wang/Documents/Project/Video-Game-by-Unreal-Engine/TripleX/main.cpp" "/mnt/c/Users/Jade Wang/Documents/Project/Video-Game-by-Unreal-Engine/TripleX/cmake-build-debug/CMakeFiles/TripleX.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TripleX/cmake-build-debug/CMakeFiles/TripleX.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TripleX.dir/main.cpp.o"
+  "TripleX.pdb"
+  "TripleX"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TripleX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
